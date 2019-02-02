a1a988fa8e64475aa917f2e30e27d5a9bdd3946c
yguozhen <47097105+yguozhen@users.noreply.github.com>
2019-02-02 18:11:34 +0800

    Add files via upload

---
+++ TestIdentifer.java
@@ -0,0 +1,13 @@
+
+public class TestIdentifer {
+	public static void main(String[] args) {
+		int a1 = 1;
+		//int 1a = 1; number can be the initial
+		int $a = 1;
+		int _a = 2;
+		// int #a = 4; symbols exclude _ and $ can not
+		int Êý×Ö = 6;
+		//int class = 7; cannot be used.
+				
+	}
+}
