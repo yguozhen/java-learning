81caa5d13138e04448cfb54e1150d684330d2c39
yguozhen <47097105+yguozhen@users.noreply.github.com>
2019-02-04 22:58:37 +0800

    Add files via upload

---
+++ Pro02/src/TestPrimitiveDataType.java
@@ -0,0 +1,22 @@
+
+public class TestPrimitiveDataType {
+
+		public static void main(String[] args) {
+			
+			//test primitive data type
+			
+			int a = 15;    //decimal system
+			int b = 015;   //octal number system
+			int c = 0b1101;//binary number 
+			int d = 0x15;  //Hexadecimal 
+			
+			
+			System.out.println(b);
+			System.out.println(c);
+			System.out.println(d);
+			
+		
+			
+		}
+	
+}
