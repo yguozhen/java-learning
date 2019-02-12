2f4a56d48bc6097505060d5c7844b0536461a3cc
yguozhen <47097105+yguozhen@users.noreply.github.com>
2019-02-12 08:25:17 +0800

    Add files via upload

---
+++ Pro02/src/TestTypeConvert.java
@@ -0,0 +1,14 @@
+/**
+ * test automatic type convert
+ * @author Administrator
+ *
+ */
+public class TestTypeConvert {
+	public static void main(String[] args) {
+		int a =324;
+		double b = a;
+		long c = a;
+		long e = 314;
+	}
+
+}
