5704021d20340258ea6a3c6f59ae50a40c258864
yguozhen <47097105+yguozhen@users.noreply.github.com>
2019-02-19 09:50:22 +0800

    Add files via upload

---
+++ Pro03/src/TestFor.java
@@ -0,0 +1,21 @@
+/**
+ * test for circulate
+ * @author Administrator
+ *
+ */
+public class TestFor {
+	public static void mian(String[] args) {
+		int sum = 0;
+		for(int i = 1;i<=100;i++) {
+			sum = sum+1;
+/*
+ * same fuction 
+ * int i = 1;
+ * while(i <= 100){
+ * sum = sum+1;
+ * i ++;
+ */
+		}
+	}
+
+}
