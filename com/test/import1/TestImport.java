8442c84c1623a84ad83df4b6da2683d2f5d96f24
yguozhen <47097105+yguozhen@users.noreply.github.com>
2019-03-13 09:44:19 +0800

    Add files via upload

---
+++ com/test/import1/TestImport.java
@@ -0,0 +1,22 @@
+package com.test.import1;
+
+import com.test.package1.User;
+
+import java.util.Date;
+import java.sql.*;
+
+import static java.lang.Math.*;
+
+
+public class TestImport {
+	public static void main(String[] args) {
+		User user = new User();
+		String str;
+		
+		java.util.Date date = new java.util.Date();
+		
+		System.out.println(Math.PI);
+		System.out.println(PI);
+	}
+
+}
