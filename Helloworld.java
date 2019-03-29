c82ee8aa666f32bbcdddf3f50c6feb6e3228c492
yguozhen <47097105+yguozhen@users.noreply.github.com>
2019-03-29 15:39:52 +0800

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author yangguozhen
+ *
+ */
+public class Helloworld {
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world\n****\n****\n");
+	}
+
+}
