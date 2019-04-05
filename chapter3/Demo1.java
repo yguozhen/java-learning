c13b89686c5e6671b419c1c172d0ecd5c36e79da
yguozhen <47097105+yguozhen@users.noreply.github.com>
2019-04-05 10:06:36 +0800

    Add files via upload

---
+++ chapter3/Demo1.java
@@ -0,0 +1,17 @@
+package com.javalearning.chapter3;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		
+	int a = 0 ;
+	if (a>0) 
+		System.out.println("a是正数");
+	
+	else if (a <0) 
+		System.out.println("a是负数");
+	
+	else
+	System.out.println("a = 0");
+	}
+
+}
