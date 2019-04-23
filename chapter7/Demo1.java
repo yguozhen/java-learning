fe987bb494b9067577cb517921a263a5a4e2e769
yguozhen <yguozhen90@gmail.com>
2019-04-23 17:13:42 +0800

    normal block

---
+++ chapter7/Demo1.java
@@ -0,0 +1,14 @@
+package com.javalearning.chapter7;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		int a = 1;
+		{
+			//normal code 
+			a = 2;
+			System.out.println(a);
+		}
+		
+	}
+
+}
